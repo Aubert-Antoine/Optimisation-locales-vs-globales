42b0b8f0b0ff7f533b5529d69dbfc075c9387a6c
Antoine Aubert <antoine0aubert@edu.esiee.fr>
2022-06-03 13:56:13 +0200

    Class Stat & RAndom 1st commit

---
+++ src/EvalStat.java
@@ -0,0 +1,10 @@
+
+
+/**
+ * EvalStat est la classe qui s'occupe des evaluations statistique des ex du projet.
+ */
+public class EvalStat {
+    public static void mainEvalStat(){
+        System.out.println("Hello, World! from EvalStat Class");
+    }
+}
---
+++ src/RandomGen.java
@@ -0,0 +1,12 @@
+import java.util.random.*;
+
+
+
+/**
+ * RandomGen est la classe qui genere alleatoirement les tableaux requis pour les runs
+ */
+public class RandomGen {
+    public static void mainRandomGen(){
+        System.out.println("Hello, World! from RandomGen Class");
+    }
+}
