c5f7e1ca08ae06d45c8a534c4a47643b5c04bc8a
Antoine Aubert <antoine0aubert@gmail.com>
2022-06-04 00:35:11 +0200

    EvalStat 2 fonction

--- src/EvalStat.java
+++ src/EvalStat.java
@@ -4,6 +4,32 @@
  * EvalStat est la classe qui s'occupe des evaluations statistique des ex du projet.
  */
 public class EvalStat {
+
+    /**
+     * evalMax retourne la distance relative entre deux solution, une optimal -> dynamique et une autre -> gloutonne
+     * @param pValDyn pram int, valeur dynamique    
+     * @param pValGlou pram int, valeur gloutonne 
+     * @return  la distance pour une rechecher de maximisation
+     */
+    public static int evalMax(int pValDyn, int pValGlou) {
+        return (pValDyn-pValGlou)/pValDyn;
+    }
+
+    /**
+     * evalMax retourne la distance relative entre deux solution, une optimal -> dynamique et une autre -> gloutonne
+     * @param pValDyn pram int, valeur dynamique    
+     * @param pValGlou pram int, valeur gloutonne 
+     * @return  la distance pour une rechecher de maximisation
+     */
+    public static int evalMin(int pValDyn, int pValGlou) {
+        return (pValDyn-pValGlou)/pValDyn;
+    }
+
+
+
+
+
+
     public static void mainEvalStat(){
         System.out.println("Hello, World! from EvalStat Class");
     }
