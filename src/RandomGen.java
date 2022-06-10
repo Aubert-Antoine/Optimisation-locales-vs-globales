35811a16d6305004a1af1f9802bd922ddcaddcf3
Your NamePauline <you@example.compauline.gouillart@gmail.com>
2022-06-10 15:13:28 +0200

    dernière version

--- src/RandomGen.java
+++ src/RandomGen.java
@@ -2,13 +2,11 @@ import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 
-
-
-
 /**
- * RandomGen est la classe qui genere alleatoirement les tableaux requis pour les runs
+ * RandomGen est la classe qui génère aléatoirement les tableaux requis pour les runs
  */
 public class RandomGen {
+
 
     public static boolean debug = false;
     public static boolean info = false;
@@ -17,17 +15,24 @@ public class RandomGen {
         System.out.println("Hello, World! from RandomGen Class");
     }
 
+
     /**
-     * randoomInt retourne un entier dans la plage indiqué (borne sup comprise)
+     * randoomInt retourne un entier dans la plage indiquée (borne sup comprise)
      * @param pMin min de la borne
      * @param pMax max de la borne (inclue)
-     * @return un int au hasard dans l'interval pMin, pMax
+     * @return un int au hasard dans l'intervalle [pMin, pMax]
      */
     public static int randomInt(int pMin, int pMax) {
         return ThreadLocalRandom.current().nextInt(pMin, pMax + 1);
     }
 
 
+    /**
+     * randomTabInt retourne un tableau d'entiers dans la plage indiquée (borne sup comprise)
+     * @param pTabLen taille du tableau
+     * @param pMaxVal max de la borne (inclue)
+     * @return un tableau d'int au hasard avec des valeurs dans l'intervalle [0, pMaxVal]
+     */
     public static int[] randomTabInt(int pTabLen, int pMaxVal) {
         int[] randomTab = new int[pTabLen];
 
@@ -38,7 +43,6 @@ public class RandomGen {
         if(info) System.out.println("RANDOM > randomTabInt :\n"+Arrays.toString(randomTab));
 
         return randomTab;
-
     }
 
 
